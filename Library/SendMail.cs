936fefa4e4d0337aa44fd254f8cba024f3a083ef
Nguyễn Thanh Duy <oslthanhduy@gmail.com>
2023-04-17 08:06:56 +0700

    ...

---
+++ Library/SendMail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class SendMail
+    {
+        public static void SendEmail(string senderEmail, string senderPassword, string recipientEmail, string subject, string body)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential(senderEmail, senderPassword);
+
+            var mailMessage = new MailMessage(senderEmail, recipientEmail, subject, body);
+            mailMessage.IsBodyHtml = true;
+
+            smtpClient.Send(mailMessage);
+        }
+
+    }
+}
---
+++ VTC_Webhook/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VTC_Webhook.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public JsonResult Getdata()
+        {
+            JsonResult jr = new JsonResult();
+
+            String dataJson= new StreamReader(Request.InputStream).ReadToEnd();
+
+            jr.Data = new
+            {
+                status = "Dữ liệu nhận thành công",
+            };
+            return Json(jr, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.partnerId = ConfigurationManager.AppSettings["PartnerId"];
+            ViewBag.secretKey = ConfigurationManager.AppSettings["SecretKey"];
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
